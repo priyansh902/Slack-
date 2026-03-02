82ba30186bc63e5b30bbc829a717986f90e95a5a
Priyanshu Kumar <pk953666@gmail.com>
2026-03-02 21:33:26 +0530

    util properties

--- src/main/java/com/PhoenixTechSolutions/product1/Security/Jwtutil.java
+++ src/main/java/com/PhoenixTechSolutions/product1/Security/Jwtutil.java
@@ -1,5 +1,62 @@
 package com.PhoenixTechSolutions.product1.Security;
 
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+
+import javax.crypto.SecretKey;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+
+
+@Component
 public class Jwtutil {
+
+    private final SecretKey key;
+    private final long expiration;
+
+        public Jwtutil(
+            @Value("${jwt.secret}") String secret,
+            @Value("${jwt.expiration}") long expiration
+    ) {
+        this.key = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+        this.expiration = expiration;
+    }
+
+    public String generateToken(String email) {
+        return Jwts.builder()
+                .setSubject(email)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + expiration))
+                .signWith(key)
+                .compact();
+    }
+
+    public String extractEmail(String token) {
+        return extractAllClaims(token).getSubject();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            extractAllClaims(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
     
 }

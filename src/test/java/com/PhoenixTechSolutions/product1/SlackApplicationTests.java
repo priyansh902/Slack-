5a3a0c66b11085fb89210e6bbfc35d4586ebc7ed
Priyanshu Kumar <pk953666@gmail.com>
2026-03-02 20:15:42 +0530

    started in 3 march

---
+++ src/test/java/com/PhoenixTechSolutions/product1/SlackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PhoenixTechSolutions.product1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

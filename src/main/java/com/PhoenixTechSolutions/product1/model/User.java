37d7d1677239d7ffec5c40efd6390bc0b2ce4989
Priyanshu Kumar <pk953666@gmail.com>
2026-03-02 20:48:03 +0530

    user

--- src/main/java/com/PhoenixTechSolutions/product1/model/User.java
+++ src/main/java/com/PhoenixTechSolutions/product1/model/User.java
@@ -1,5 +1,31 @@
 package com.PhoenixTechSolutions.product1.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
+    private String id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
     
 }

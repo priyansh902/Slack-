bbfea8697adf9312cf093eca2acd58bd445aa986
Priyanshu Kumar <pk953666@gmail.com>
2026-03-03 18:48:23 +0530

    mapping

--- src/main/java/com/PhoenixTechSolutions/product1/controllers/UserController.java
+++ src/main/java/com/PhoenixTechSolutions/product1/controllers/UserController.java
@@ -1,5 +1,11 @@
 package com.PhoenixTechSolutions.product1.controllers;
 
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+@RestController
+@RequestMapping("/api/users")
 public class UserController {
+
+    
     
 }

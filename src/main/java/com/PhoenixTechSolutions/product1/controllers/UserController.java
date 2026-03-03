1db004bd872da9ca7b533e94f4f2a9cc32a171d6
Priyanshu Kumar <pk953666@gmail.com>
2026-03-03 19:29:37 +0530

    user api's

--- src/main/java/com/PhoenixTechSolutions/product1/controllers/UserController.java
+++ src/main/java/com/PhoenixTechSolutions/product1/controllers/UserController.java
@@ -15,7 +15,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;

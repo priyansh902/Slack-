81763093b31b01576b2120e6b6d8c33204f2c180
Priyanshu Kumar <pk953666@gmail.com>
2026-03-03 15:06:28 +0530

    implement custom userdetailService

--- src/main/java/com/PhoenixTechSolutions/product1/Security/UserdetailsService.java
+++ src/main/java/com/PhoenixTechSolutions/product1/Security/UserdetailsService.java
@@ -1,5 +1,28 @@
 package com.PhoenixTechSolutions.product1.Security;
 
-public class UserdetailsService {
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.PhoenixTechSolutions.product1.repositiory.UserRepositiory;
+
+@Service
+public class UserdetailsService implements UserDetailsService {
+
+    private final UserRepositiory userRepository;
+
+    public UserdetailsService(UserRepositiory userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email)
+            throws UsernameNotFoundException {
+
+        return userRepository.findByEmail(email.toLowerCase())
+                .orElseThrow(() ->
+                        new UsernameNotFoundException("User not found: " + email));
+    }
     
 }
--- src/main/java/com/PhoenixTechSolutions/product1/repositiory/UserRepositiory.java
+++ src/main/java/com/PhoenixTechSolutions/product1/repositiory/UserRepositiory.java
@@ -1,10 +1,12 @@
 package com.PhoenixTechSolutions.product1.repositiory;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.PhoenixTechSolutions.product1.model.User;
 
 public interface UserRepositiory extends JpaRepository<User, Long> {
-    User findByEmail(String email);
+   Optional<User> findByEmail(String email);
     
 }

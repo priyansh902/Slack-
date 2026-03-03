31341134e1a129e1af07107f3962d1b5e13fb862
Priyanshu Kumar <pk953666@gmail.com>
2026-03-03 19:17:07 +0530

    Register dti

--- src/main/java/com/PhoenixTechSolutions/product1/model/User.java
+++ src/main/java/com/PhoenixTechSolutions/product1/model/User.java
@@ -13,11 +13,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class User implements UserDetails {
 
     @Id

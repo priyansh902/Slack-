3772398400960301c8592dfe8cfcc9838d24a030
Priyanshu Kumar <pk953666@gmail.com>
2026-03-04 19:20:09 +0530

    User done / Profile entity

--- src/main/java/com/PhoenixTechSolutions/product1/model/Profile.java
+++ src/main/java/com/PhoenixTechSolutions/product1/model/Profile.java
@@ -1,5 +1,20 @@
 package com.PhoenixTechSolutions.product1.model;
 
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Profile {
+
+
     
 }

50de1998c155870e08469140285d1221bebf193b
Priyanshu Kumar <pk953666@gmail.com>
2026-03-04 20:07:12 +0530

    dto -> record / Fields

--- src/main/java/com/PhoenixTechSolutions/product1/Dtos/ProfileRequest.java
+++ src/main/java/com/PhoenixTechSolutions/product1/Dtos/ProfileRequest.java
@@ -1,5 +1,19 @@
 package com.PhoenixTechSolutions.product1.Dtos;
 
-public record ProfileRequest() {
+import org.hibernate.validator.constraints.URL;
+
+import jakarta.validation.constraints.Size;
+
+public record ProfileRequest(
+    
+    @Size(max = 500, message = "Bio cannot exceed 500 characters")
+    String bio,
+    
+    @URL(message = "Invalid GitHub URL format")
+    String githubUrl,
+    
+    @URL(message = "Invalid LinkedIn URL format")
+    String linkedinUrl
+) {
     
 }

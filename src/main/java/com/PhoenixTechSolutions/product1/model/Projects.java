5789c3d8756bb5d04651dad85859a98f156661a2
Priyanshu Kumar <pk953666@gmail.com>
2026-03-05 16:56:30 +0530

    model / projects

--- src/main/java/com/PhoenixTechSolutions/product1/model/Projects.java
+++ src/main/java/com/PhoenixTechSolutions/product1/model/Projects.java
@@ -1,10 +1,51 @@
 package com.PhoenixTechSolutions.product1.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "projects")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Projects {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(length = 1000)
+    private String description;
+
+    @Column(name = "tech_stack", length = 500)
+    private String techStack;  
+
+    @Column(name = "github_link")
+    private String githubLink;
+
+    @Column(name = "live_link")
+    private String liveLink;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
 }

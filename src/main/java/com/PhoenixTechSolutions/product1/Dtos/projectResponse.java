316c20f3f3fc5b6416fcffd4b1724a6ab9b2ea54
Priyanshu Kumar <pk953666@gmail.com>
2026-03-05 18:35:28 +0530

    Response

--- src/main/java/com/PhoenixTechSolutions/product1/Dtos/projectResponse.java
+++ src/main/java/com/PhoenixTechSolutions/product1/Dtos/projectResponse.java
@@ -1,5 +1,16 @@
 package com.PhoenixTechSolutions.product1.Dtos;
 
-public record projectResponse() {
-    
-}
+import java.time.LocalDateTime;
+
+public record ProjectResponse(
+    Long id,
+    Long userId,
+    String username,
+    String title,
+    String description,
+    String techStack,
+    String githubLink,
+    String liveLink,
+    LocalDateTime createdAt,
+    LocalDateTime updatedAt
+) {}

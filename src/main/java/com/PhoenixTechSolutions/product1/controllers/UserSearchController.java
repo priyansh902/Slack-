a92f91961d89086e86820935a5582ac9113a653f
Priyanshu Kumar <pk953666@gmail.com>
2026-03-05 08:18:27 +0530

    User search Functionality

---
+++ src/main/java/com/PhoenixTechSolutions/product1/controllers/UserSearchController.java
@@ -0,0 +1,8 @@
+package com.PhoenixTechSolutions.product1.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserSearchController {
+    
+}

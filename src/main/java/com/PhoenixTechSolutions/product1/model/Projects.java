0f32e9210c9930e2dfd229a682fda464a440ece4
Priyanshu Kumar <pk953666@gmail.com>
2026-03-05 16:48:52 +0530

    Project section:

--- src/main/java/com/PhoenixTechSolutions/product1/model/Projects.java
+++ src/main/java/com/PhoenixTechSolutions/product1/model/Projects.java
@@ -1,5 +1,10 @@
 package com.PhoenixTechSolutions.product1.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "projects")
 public class Projects {
     
 }

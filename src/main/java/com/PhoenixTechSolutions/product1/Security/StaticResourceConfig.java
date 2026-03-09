cba61facbb73fb6c64dac695423aa1602e13c9ce
Priyanshu Kumar <pk953666@gmail.com>
2026-03-09 10:06:19 +0530

    resource config Base form

--- src/main/java/com/PhoenixTechSolutions/product1/Security/StaticResourceConfig.java
+++ src/main/java/com/PhoenixTechSolutions/product1/Security/StaticResourceConfig.java
@@ -1,5 +1,22 @@
 package com.PhoenixTechSolutions.product1.Security;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+
+import jakarta.validation.Valid;
+
+
+
+@Configuration
 public class StaticResourceConfig {
-    
+   
+    @Valid()
+    private String uploadDir;
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/uploads/**")
+                .addResourceLocations("file:" + uploadDir + "/");
+    }
+
 }

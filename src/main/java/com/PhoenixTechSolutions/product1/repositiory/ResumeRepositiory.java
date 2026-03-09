2e4df0c5b6b1be70206df94a8b1b421f259667c9
Priyanshu Kumar <pk953666@gmail.com>
2026-03-09 10:13:56 +0530

    Resume quieries and methods in Repositiory

--- src/main/java/com/PhoenixTechSolutions/product1/repositiory/ResumeRepositiory.java
+++ src/main/java/com/PhoenixTechSolutions/product1/repositiory/ResumeRepositiory.java
@@ -1,5 +1,32 @@
 package com.PhoenixTechSolutions.product1.repositiory;
 
-public class ResumeRepositiory {
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.security.core.parameters.P;
+import org.springframework.stereotype.Repository;
+
+import com.PhoenixTechSolutions.product1.model.Resume;
+
+@Repository
+public interface ResumeRepositiory extends JpaRepository<Resume, Long> {
+     Optional <Resume> findByUserId(Long userId);
+
+     @Query("SELECT r FROM Resume r JOIN r.user u WHERE u.username = :username")
+     Optional <Resume> findByUsername(@P("username") String username);
+
+     // Check if a resume exists for a given user ID
+     boolean exiexistsByUserId(Long userId);
+
+     // Retrieve all resumes (Admin functionality)
+     List<Resume> findAll();
+
+     // Count the total number of resumes (Admin functionality)
+     long count();
+
+    // Delete a resume by user ID (Admin functionality & User functionality)
+     void deleteByUserId(Long userId);
     
 }

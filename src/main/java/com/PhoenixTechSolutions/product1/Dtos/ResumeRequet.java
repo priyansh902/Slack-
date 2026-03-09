5811e11eb3653d5e794e11878cf2d3d207f493c1
Priyanshu Kumar <pk953666@gmail.com>
2026-03-09 10:16:44 +0530

    upload dto / request

--- src/main/java/com/PhoenixTechSolutions/product1/Dtos/ResumeRequet.java
+++ src/main/java/com/PhoenixTechSolutions/product1/Dtos/ResumeRequet.java
@@ -1,5 +1,14 @@
 package com.PhoenixTechSolutions.product1.Dtos;
 
-public record ResumeRequet() {
+import org.springframework.web.multipart.MultipartFile;
+
+import jakarta.validation.constraints.NotNull;
+
+public record ResumeRequet(
+    @NotNull(
+        message = "Resume file must not be null"
+    )
+    MultipartFile file
+) {
     
 }

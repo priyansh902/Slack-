64c4628a76727bbb3fedd8bd612f919dcaaa9f8f
Priyanshu Kumar <pk953666@gmail.com>
2026-03-09 10:19:32 +0530

    response of resume

--- src/main/java/com/PhoenixTechSolutions/product1/Dtos/ResumeResponse.java
+++ src/main/java/com/PhoenixTechSolutions/product1/Dtos/ResumeResponse.java
@@ -1,5 +1,17 @@
 package com.PhoenixTechSolutions.product1.Dtos;
 
-public record ResumeResponse() {
+import java.time.LocalDateTime;
+
+public record ResumeResponse(
+    Long id,
+    Long userId,
+    Long username,
+    String fileName,
+    String fileUrl,
+    Long fileSize,
+    String contentType,
+    LocalDateTime uploadedAt,
+    LocalDateTime updatedAt
+) {
     
 }

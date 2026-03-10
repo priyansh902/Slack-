1a8551308a18d84a5346c1ab093a2a8a6131814f
Priyanshu Kumar <pk953666@gmail.com>
2026-03-10 20:10:44 +0530

    response dto Fields

--- src/main/java/com/PhoenixTechSolutions/product1/Dtos/PublicPortfolioResponse.java
+++ src/main/java/com/PhoenixTechSolutions/product1/Dtos/PublicPortfolioResponse.java
@@ -1,5 +1,21 @@
 package com.PhoenixTechSolutions.product1.Dtos;
 
-public record PublicPortfolioResponse() {
+import java.time.LocalDateTime;
+import java.util.List;
+
+public record PublicPortfolioResponse(
+    String username,
+    String name,
+    String email,  
+    String bio,
+    List<String> skills,
+    String githubUrl,
+    String linkedinUrl,
+    List<PortfolioprojectDto> projects,
+    String resumeUrl,
+    int projectCount,
+    LocalDateTime memberSince,
+    LocalDateTime lastUpdated
+) {
     
 }

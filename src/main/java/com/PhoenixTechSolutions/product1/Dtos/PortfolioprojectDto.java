6e618f6fd01baf4fb26f7cc55b8119868e675bb0
Priyanshu Kumar <pk953666@gmail.com>
2026-03-10 20:08:50 +0530

    Portfolio Dto

--- src/main/java/com/PhoenixTechSolutions/product1/Dtos/PortfolioprojectDto.java
+++ src/main/java/com/PhoenixTechSolutions/product1/Dtos/PortfolioprojectDto.java
@@ -1,5 +1,12 @@
 package com.PhoenixTechSolutions.product1.Dtos;
 
-public record PortfolioprojectDto() {
+public record PortfolioprojectDto(
+    Long id,
+    String title,
+    String description,
+    String techStack,
+    String githubLink,
+    String liveLink
+) {
     
 }
